e67828e9d54c11950c37a6f8d8effb6acffc7b97
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2023-02-12 18:18:45 +0100

    postbuild command removed for compatibility

--- vs-cpp/04/premake5.lua
+++ vs-cpp/04/premake5.lua
@@ -1,12 +1,12 @@
-project "04"  
-    kind "ConsoleApp" 
+project "04"
+    kind "ConsoleApp"
     language "C++"
     cppdialect "C++17"
     staticruntime "off"
 
     targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
-    
+
     files {
         "src/**.h",
         "src/**.cpp",
@@ -15,10 +15,6 @@ project "04"
 
     debugargs {
         "../inputs/forest01.txt", "../results/forest01.txt.out"
-    }
-
-    postbuildcommands {
-        '{COPYFILE} "%{cfg.buildtarget.relpath}" "%{wks.location}exe/%{cfg.buildtarget.basename}_%{cfg.buildcfg}.exe"'
     }
 
     filter "configurations:Debug"
